dcabec552541589af6ac2804db61cfa80ad3f8b5
Nishal <nishalkkn@gmail.com>
2024-08-30 15:39:23 +0530

    aug 30

--- sale_order_payment/__manifest__.py
+++ sale_order_payment/__manifest__.py
@@ -2,10 +2,13 @@
     'name': 'Sale order payment',
     'application': True,
     'version': '17.0.1.0.1',
+    'summary': 'Sale order payment',
+    'description': """
+This is a base module for show the register payment button in sale order and make register the payment from sale order
+    """,
 
     'depends': [
         'sale',
-        'account',
     ],
     'data': [
         'views/sale_order_payment.xml'
--- sale_order_payment/models/sale_order_payment.py
+++ sale_order_payment/models/sale_order_payment.py
@@ -4,19 +4,44 @@ from odoo import models, _, fields, api
 class SaleOrderPayment(models.Model):
     _inherit = 'sale.order'
 
-    # invoice_state = fields.Char(default="def")
+    payment = fields.Boolean(default=False)
+    button_state = fields.Selection(selection=[('hidden', 'Hidden'), ('visible', 'Visible')], required=True, copy=False,
+                                    store=True, tracking=True, default='hidden', compute='_compute_button_state')
+    fully_paid_ribbon = fields.Boolean(default=False)
+    partial_paid_ribbon = fields.Boolean(default=False)
 
     def action_register_payment(self):
-        if self.invoice_ids.state == 'posted':
-            return {
-                'name': _('Register Payment'),
-                'res_model': 'account.payment.register',
-                'view_mode': 'form',
-                'views': [[False, 'form']],
-                'context': {
-                    'active_model': 'account.move',
-                    'active_ids': self.invoice_ids.id,
-                },
-                'target': 'new',
-                'type': 'ir.actions.act_window',
-            }
+        """button for register payment in sale order"""
+        return {
+            'name': _('Register Payment'),
+            'res_model': 'account.payment.register',
+            'view_mode': 'form',
+            'views': [[False, 'form']],
+            'context': {
+                'active_model': 'account.move',
+                'active_ids': self.invoice_ids.ids,
+            },
+            'target': 'new',
+            'type': 'ir.actions.act_window',
+        }
+
+    @api.depends('invoice_ids.payment_state', 'invoice_ids.state')
+    def _compute_button_state(self):
+        """computing the button state"""
+        if self.invoice_ids:
+            var = self.invoice_ids.mapped('payment_state')
+            for rec in self.invoice_ids:
+                if rec.state == 'posted':
+                    self.write({
+                        'button_state': 'visible'
+                    })
+                if 'paid' in var:
+                    self.write({
+                        'partial_paid_ribbon': True
+                    })
+            if 'paid' in var and 'not_paid' not in var:
+                self.write({
+                    'button_state': 'hidden',
+                    'partial_paid_ribbon': False,
+                    'fully_paid_ribbon': True
+                })
